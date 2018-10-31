4ffbf3ae35377d2c16d636df628b33ab23beb5a1
Adam Gruer <adamgruer@icloud.com>
2018-10-31 15:21:36 +1100

    add additional input validaiton

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -3,6 +3,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import String exposing (length)
+import Regex exposing (..)
 
 
 
@@ -19,6 +20,7 @@ main =
 
 type alias Model =
   { name : String
+  , age : String
   , password : String
   , passwordAgain : String
   }
@@ -26,7 +28,7 @@ type alias Model =
 
 init : Model
 init =
-  Model "" "" ""
+  Model "" "" "" ""
 
 
 
@@ -35,6 +37,7 @@ init =
 
 type Msg
   = Name String
+  | Age String
   | Password String
   | PasswordAgain String
 
@@ -44,7 +47,8 @@ update msg model =
   case msg of
     Name name ->
       { model | name = name }
-
+    Age age ->
+      { model | age = age }
     Password password ->
       { model | password = password }
 
@@ -60,6 +64,7 @@ view : Model -> Html Msg
 view model =
   div []
     [ viewInput "text" "Name" model.name Name
+    , viewInput "text" "Age" model.age Age
     , viewInput "password" "Password" model.password Password
     , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
     , viewValidation model
@@ -73,9 +78,47 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if length model.password < 8 then
+
+  div []  
+  [
+    if not(Regex.contains lowerCase model.password) then
+       div [ style "color" "red" ] [ text "Password needs at lease one lower case character" ]
+    else div [][],
+    if not(Regex.contains upperCase model.password) then
+       div [ style "color" "red" ] [ text "Password needs at lease one upper case character" ]
+       else div [][],
+    if not(Regex.contains numeric model.password) then
+       div [ style "color" "red" ] [ text "Password needs at lease one numeric character" ]
+       else div [][],
+    if  Regex.contains nonNumeric model.age  then
+      div [ style "color" "red" ] [ text "Age needs to be a number" ]
+      else div [][],
+    if length model.password < 8 then
     div [ style "color" "red" ] [ text "Password needs to be eight characters or more" ]
-  else if  model.password == model.passwordAgain then
+    else div [][],
+    if  model.password == model.passwordAgain then
     div [ style "color" "green" ] [ text "OK" ]
   else
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
+  ]
+
+
+lowerCase : Regex.Regex
+lowerCase = 
+  Maybe.withDefault Regex.never <|
+    Regex.fromString "[a-z]+"
+
+upperCase : Regex.Regex
+upperCase = 
+  Maybe.withDefault Regex.never <|
+    Regex.fromString "[A-Z]+"
+
+numeric : Regex.Regex
+numeric = 
+  Maybe.withDefault Regex.never <|
+    Regex.fromString "[0-9]+"
+
+nonNumeric : Regex.Regex
+nonNumeric = 
+  Maybe.withDefault Regex.never <|
+    Regex.fromString "[^0-9]+"
